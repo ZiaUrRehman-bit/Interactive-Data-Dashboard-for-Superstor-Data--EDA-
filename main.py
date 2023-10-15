bf5b7b14f5b0e4628bc10b209d79992d53f3c21d
hp <engrziaurrehman.kicsit@gmail.com>
2023-10-15 23:40:12 +0500

    update

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import plotly.express as px
 import pandas as pd
 import os               # for file handling
 import warnings         # incase there is some kind of warring’s so i want to ignore those
+import matplotlib
 warnings.filterwarnings('ignore')
 
 # (2) Page Configuration and Dashboard Tittle
@@ -98,3 +99,43 @@ with col2:
     fig = px.pie(filtered_df, values = "Sales", names = "Region", hole = 0.5)
     fig.update_traces(text = filtered_df["Region"], textposition = "outside")
     st.plotly_chart(fig,use_container_width=True)
+
+# (6)
+
+filtered_df["month_year"] = filtered_df["Order Date"].dt.to_period("M")
+st.subheader('Time Series Analysis')
+
+linechart = pd.DataFrame(filtered_df.groupby(filtered_df["month_year"].dt.strftime("%Y : %b"))["Sales"].sum()).reset_index()
+fig2 = px.line(linechart, x = "month_year", y="Sales", labels = {"Sales": "Amount"},height=500, width = 1000,template="gridon")
+st.plotly_chart(fig2,use_container_width=True)
+
+with st.expander("View Data of TimeSeries:"):
+    st.write(linechart.T)
+    csv = linechart.to_csv(index=False).encode("utf-8")
+    st.download_button('Download Data', data = csv, file_name = "TimeSeries.csv", mime ='text/csv')
+
+import plotly.figure_factory as ff
+st.subheader(":point_right: Month wise Sub-Category Sales Summary")
+with st.expander("Summary_Table"):
+    df_sample = df[0:5][["Region","State","City","Category","Sales","Profit","Quantity"]]
+    fig = ff.create_table(df_sample, colorscale = "Cividis")
+    st.plotly_chart(fig, use_container_width=True)
+
+    st.markdown("Month wise sub-Category Table")
+    filtered_df["month"] = filtered_df["Order Date"].dt.month_name()
+    sub_category_Year = pd.pivot_table(data = filtered_df, values = "Sales", index = ["Sub-Category"],columns = "month")
+    st.write(sub_category_Year)
+
+# Create a scatter plot
+data1 = px.scatter(filtered_df, x = "Sales", y = "Profit", size = "Quantity")
+data1['layout'].update(title="Relationship between Sales and Profits using Scatter Plot.",
+                       titlefont = dict(size=20),xaxis = dict(title="Sales",titlefont=dict(size=19)),
+                       yaxis = dict(title = "Profit", titlefont = dict(size=19)))
+st.plotly_chart(data1,use_container_width=True)
+
+with st.expander("View Data"):
+    st.write(filtered_df.iloc[:500,1:20:2])
+
+# Download orginal DataSet
+csv = df.to_csv(index = False).encode('utf-8')
+st.download_button('Download Data', data = csv, file_name = "Data.csv",mime = "text/csv")
